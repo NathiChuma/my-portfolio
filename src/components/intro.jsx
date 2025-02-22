db8a1657d324c7d5e272b21c1d445ff3bc49f232
Nkosinathi Chuma <nuttychuma@gmail.com>
2025-02-22 20:46:02 +0200

    fixed bg image

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import "./stars.scss";
 import {ReactTyped} from "react-typed";
 
-//import backgroundImage from "../img/Ghost.JPG";
 import backgroundImage from "../img/NathiWB.jpg";
 
-//https://t4.ftcdn.net/jpg/10/16/77/45/240_F_1016774590_USkqd3QYi73RJzIgAEeNg1kWIIG41Rz9.jpg
-//<div id="home" className="intro route bg-image background">
 class Intro extends React.Component {
   render() {
     return (
-       <div id="home" className="intro route bg-image" style={{backgroundImage: "url(" + backgroundImage + ")"}}>
+       <div id="home" className="intro route bg-image" style={{backgroundImage: "url(" + backgroundImage + ")",}}>
         <div id="stars" />
         <div id="stars2" />
         <div id="stars3" />
@@ -18,7 +15,7 @@ class Intro extends React.Component {
         <div className="intro-content display-table">
           <div className="table-cell">
             <div className="container">
-              <h1 className="intro-title mb-4">Hello, I am Nkosinath Chuma</h1>
+              <h1 className="intro-title mb-4">Hello, I am Nkosinathi Chuma</h1>
               <p className="intro-subtitle">
                 <span className="text-slider-items"></span>
                 <strong className="text-slider">
@@ -27,7 +24,7 @@ class Intro extends React.Component {
                       "Front End Developer",
                       "Back End Developer",
                       "Software Engineer",
-                      "Chill Guy"
+                      "Chill Guy 🙂"
                     ]}
                     typeSpeed={80}
                     backDelay={1100}
